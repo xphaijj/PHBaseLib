8cbc96ecac48a6b48087d7238cf942d28ef28f6b
項普華 <2112787533@qq.com>
2017-08-07 21:13:43 +0800

    0.1.1

--- PHBaseLib.podspec
+++ PHBaseLib.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'PHBaseLib'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = '自定义基础类库'
 
   s.description      = <<-DESC
--- PHBaseLib/Classes/Utils/PHKeyMarco.h
+++ PHBaseLib/Classes/Utils/PHKeyMarco.h
@@ -10,19 +10,19 @@
 #define PHKeyMarco_h
 
 /**************友盟****************/
-#define USHARE_APPKEY @"596f442504e2051656001c56"
+#define USHARE_APPKEY @""
 /**************微信****************/
-#define WECHAT_APPKEY @"wx42e0d734692aae6a"
-#define WECHAT_APPSECRET @"be3028b33ab1e7c99e479b3bf41a55fb"
-#define WECHAT_REDIRECT_URL @"https://www.lsd1888.com"
+#define WECHAT_APPKEY @""
+#define WECHAT_APPSECRET @""
+#define WECHAT_REDIRECT_URL @""
 
 
 /**************极光****************/
-#define JPUSH_KEY @"21a83e299757d58760338d84"
-#define JPUSH_SECRET @"5b786f296410db34e78bad20"
+#define JPUSH_KEY @""
+#define JPUSH_SECRET @""
 
 /**************百度地图****************/
-#define BAIDU_MAP_KEY @"YYwQquxKctGYGaWFhniXs7EtG639klmY"
+#define BAIDU_MAP_KEY @""
 
 
 
